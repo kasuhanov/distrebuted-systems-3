f17a623089c74d2e9758c3c944e7911a7361f489
xtal <kasuhanov@gmail.com>
2016-05-28 10:12:14 +0500

    +

--- src/main/java/ru/xtal/CsvController.java
+++ src/main/java/ru/xtal/CsvController.java
@@ -9,12 +9,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import redis.clients.jedis.Jedis;
 
 import java.io.*;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -26,7 +32,20 @@ public class CsvController {
     @RequestMapping(value = "/top", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Object getTop(@RequestParam(required = true) Long count,
                          @RequestParam(required = true) String field){
-        return field;
+        String query = "SELECT * FROM test order by "+field+" desc limit "+count+";";
+        List<String> result = jdbcTemplate.query(query, new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet resultSet, int i) throws SQLException {
+                String result= "";
+                ResultSetMetaData rsmd = resultSet.getMetaData();
+                for(int y=1;y<=rsmd.getColumnCount();y++) {
+                    result+=","+resultSet.getString(rsmd.getColumnName(y));
+                }
+                return result;
+            }
+        });
+
+        return result;
     }
 
     @RequestMapping(value = "/upload",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -39,7 +58,7 @@ public class CsvController {
 
             jdbcTemplate.execute("DROP TABLE IF EXISTS test;");
 
-            Jedis jedis = new Jedis("localhost");
+            Jedis jedis = new Jedis("redis://redistogo:4477bf16103cb12b17951d945dbd4834@lab.redistogo.com:9771/");
             jedis.rpush("queue", "downloaded/" + file.getOriginalFilename());
             return new ResponseEntity<>(HttpStatus.OK);
         }
